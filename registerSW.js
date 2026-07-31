c2a60ed5d398fd8a07c5016ddaee125d4a9881b5
lunar-byte <realcapper2018@gmail.com>
2026-07-31 11:33:39 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/MuslimAppSite/sw.js', { scope: '/MuslimAppSite/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
